09c69997e179c4e7cf967cfae6bbf89addf35dca
Jared Williams <jared@themovesapp.com>
2019-09-16 18:29:11 -0400

    Added Spinny colors..yay ;)

--- ArouseRadio/View/LiveLabel.swift
+++ ArouseRadio/View/LiveLabel.swift
@@ -8,32 +8,18 @@
 
 import Foundation
 import UIKit
-import Lottie
 
 class LiveLabel: UILabel {
-    
-    private lazy var liveIcon: LOTAnimationView = {
-        let view = LOTAnimationView(name: "circle_red_button")
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black
-        return view
-    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.addSubview(self.liveIcon)
         self.setupConstraints()
         
-        self.liveIcon.play()
-        self.liveIcon.loopAnimation = true
-    }
+   }
     
     func setupConstraints() {
-        self.liveIcon.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
-        self.liveIcon.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 0).isActive = true
-        self.liveIcon.widthAnchor.constraint(equalToConstant: 35).isActive = true
-        self.liveIcon.heightAnchor.constraint(equalToConstant: 35).isActive = true
+
     }
     
     required init?(coder aDecoder: NSCoder) {
